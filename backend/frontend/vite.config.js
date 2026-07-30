758b88b915241119bf279ce139b8dbbe2d8529b2
Ogaga Ejairu <ejairuogaga@gmail.com>
2026-07-30 16:44:45 +0100

    Fix vite outDir to default dist/ inside frontend folder

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../public/dist',
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       '/api': {
